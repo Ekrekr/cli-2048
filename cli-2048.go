f2cd73c808bcb41e98588ea4fcf1c1411cda2b56
Ekrekr <eliaskassell@gmail.com>
2021-11-23 13:35:13 +0000

    Make prettier, add new game

--- cli-2048.go
+++ cli-2048.go
@@ -5,6 +5,7 @@ import (
 	"math/rand"
 	"os"
 	"os/exec"
+	"strconv"
 	"time"
 
 	"github.com/eiannone/keyboard"
@@ -13,8 +14,10 @@ import (
 
 const (
 	TOP    = 1
+	UP     = 1
 	RIGHT  = 2
 	BOTTOM = 3
+	DOWN   = 3
 	LEFT   = 4
 )
 
@@ -25,19 +28,23 @@ var testGrid = [][]int{
 	{0, 128, 2048, 32768}}
 
 type game struct {
-	currentScore int
-	highScore    int
-	grid         [][]int
-	newGrid      [][]int
-	isGameOver   bool
+	currentScore      int
+	highScore         int
+	grid              [][]int
+	newGrid           [][]int
+	isGameOver        bool
+	needScreenRefresh bool
 }
 
 func main() {
 	rand.Seed(time.Now().UnixNano())
 
-	// args := os.Args[1:]
-
-	var g = game{currentScore: 2048, highScore: 4096, grid: testGrid, isGameOver: false}
+	var g = game{
+		currentScore:      2048,
+		highScore:         84096,
+		grid:              testGrid,
+		isGameOver:        false,
+		needScreenRefresh: true}
 
 	if err := keyboard.Open(); err != nil {
 		panic(err)
@@ -48,73 +55,114 @@ func main() {
 
 	var play = true
 	for play {
-		clearScreen()
-		g.print()
+		if g.needScreenRefresh {
+			clearScreen()
+			g.print()
+			g.needScreenRefresh = false
+		}
 		_, key, err := keyboard.GetKey()
 		if err != nil {
-			panic(err)
+			// TODO: Shift arrow keys will panic here. Ignore, or not?
+			// panic(err)
 		}
 		if g.isGameOver == true {
 			play = false
 		}
-		if key == keyboard.KeyArrowUp {
-			g.moveUp()
-		}
-		if key == keyboard.KeyArrowDown {
-			g.moveDown()
-		}
-		if key == keyboard.KeyArrowLeft {
-			g.moveLeft()
-		}
-		if key == keyboard.KeyArrowRight {
-			g.moveRight()
-		}
-		if key == keyboard.KeyCtrlQ || key == keyboard.KeyCtrlC {
+		switch key {
+		case keyboard.KeyArrowUp:
+			if g.canMove(UP) {
+				g.move(UP)
+				g.needScreenRefresh = true
+			}
+		case keyboard.KeyArrowDown:
+			if g.canMove(DOWN) {
+				g.move(DOWN)
+				g.needScreenRefresh = true
+			}
+		case keyboard.KeyArrowLeft:
+			if g.canMove(LEFT) {
+				g.move(LEFT)
+				g.needScreenRefresh = true
+			}
+		case keyboard.KeyArrowRight:
+			if g.canMove(RIGHT) {
+				g.move(RIGHT)
+				g.needScreenRefresh = true
+			}
+		case keyboard.KeyCtrlN:
+			g.newGame()
+			g.needScreenRefresh = true
+		case keyboard.KeyCtrlQ:
+		case keyboard.KeyCtrlC:
 			play = false
 		}
-		g.isGameOver = g.checkGameOver()
+		g.isGameOver = g.checkIsGameOver()
 	}
-
 }
 
-func (g *game) moveDown() {
-	for y := g.gridHeight() - 1; y > 0; y-- {
-		for x := 0; x < g.gridWidth(); x++ {
-			g.combineTilesIfEqual(y, x, y-1, x)
-			g.moveTileIfSpaceEmpty(y, x, y-1, x)
-		}
-	}
-	g.spawnNewTileIfNeeded(TOP)
-}
-
-func (g *game) moveUp() {
-	for y := 0; y < g.gridHeight()-1; y++ {
-		for x := 0; x < g.gridWidth(); x++ {
-			g.combineTilesIfEqual(y, x, y+1, x)
-			g.moveTileIfSpaceEmpty(y, x, y+1, x)
-		}
-	}
-	g.spawnNewTileIfNeeded(BOTTOM)
-}
-
-func (g *game) moveRight() {
+func (g *game) canMove(direction int) bool {
 	for y := 0; y < g.gridHeight(); y++ {
-		for x := g.gridWidth() - 1; x > 0; x-- {
-			g.combineTilesIfEqual(y, x, y, x-1)
-			g.moveTileIfSpaceEmpty(y, x, y, x-1)
+		for x := 0; x < g.gridWidth(); x++ {
+			if direction == UP && y > 0 &&
+				(g.grid[y][x] == g.grid[y-1][x] || g.grid[y-1][x] == 0) {
+				return true
+			}
+			if direction == DOWN && y < g.gridHeight()-1 &&
+				(g.grid[y][x] == g.grid[y+1][x] || g.grid[y+1][x] == 0) {
+				return true
+			}
+			if direction == LEFT && x > 0 &&
+				(g.grid[y][x] == g.grid[y][x-1] || g.grid[y][x-1] == 0) {
+				return true
+			}
+			if direction == RIGHT && x < g.gridWidth()-1 &&
+				(g.grid[y][x] == g.grid[y][x+1] || g.grid[y][x+1] == 0) {
+				return true
+			}
 		}
 	}
-	g.spawnNewTileIfNeeded(LEFT)
+	return false
 }
 
-func (g *game) moveLeft() {
-	for y := 0; y < g.gridHeight(); y++ {
-		for x := 0; x < g.gridWidth()-1; x++ {
-			g.combineTilesIfEqual(y, x, y, x+1)
-			g.moveTileIfSpaceEmpty(y, x, y, x+1)
+func (g *game) checkIsGameOver() bool {
+	return !g.canMove(UP) && !g.canMove(DOWN) && !g.canMove(LEFT) && !g.canMove(RIGHT)
+}
+
+func (g *game) move(direction int) {
+	switch direction {
+	case DOWN:
+		for y := g.gridHeight() - 1; y > 0; y-- {
+			for x := 0; x < g.gridWidth(); x++ {
+				g.combineTilesIfEqual(y, x, y-1, x)
+				g.moveTileIfSpaceEmpty(y, x, y-1, x)
+			}
 		}
+		g.spawnNewTileIfNeeded(TOP)
+	case UP:
+		for y := 0; y < g.gridHeight()-1; y++ {
+			for x := 0; x < g.gridWidth(); x++ {
+				g.combineTilesIfEqual(y, x, y+1, x)
+				g.moveTileIfSpaceEmpty(y, x, y+1, x)
+			}
+		}
+		g.spawnNewTileIfNeeded(BOTTOM)
+	case RIGHT:
+		for y := 0; y < g.gridHeight(); y++ {
+			for x := g.gridWidth() - 1; x > 0; x-- {
+				g.combineTilesIfEqual(y, x, y, x-1)
+				g.moveTileIfSpaceEmpty(y, x, y, x-1)
+			}
+		}
+		g.spawnNewTileIfNeeded(LEFT)
+	case LEFT:
+		for y := 0; y < g.gridHeight(); y++ {
+			for x := 0; x < g.gridWidth()-1; x++ {
+				g.combineTilesIfEqual(y, x, y, x+1)
+				g.moveTileIfSpaceEmpty(y, x, y, x+1)
+			}
+		}
+		g.spawnNewTileIfNeeded(RIGHT)
 	}
-	g.spawnNewTileIfNeeded(RIGHT)
 }
 
 func (g *game) combineTilesIfEqual(yTo int, xTo int, yFrom int, xFrom int) {
@@ -160,41 +208,17 @@ func (g *game) spawnNewTileIfNeeded(side int) {
 
 		var randomIndex = int32(rand.Float64() * float64(len(emptySpaces)))
 		var newTilePlace = emptySpaces[randomIndex]
-		if side == TOP {
+		switch side {
+		case TOP:
 			g.grid[0][newTilePlace] = newTileValue
-		}
-		if side == BOTTOM {
+		case BOTTOM:
 			g.grid[g.gridHeight()-1][newTilePlace] = newTileValue
-		}
-		if side == LEFT {
+		case LEFT:
 			g.grid[newTilePlace][0] = newTileValue
-		}
-		if side == RIGHT {
+		case RIGHT:
 			g.grid[newTilePlace][g.gridWidth()-1] = newTileValue
 		}
 	}
-}
-
-func (g *game) checkGameOver() bool {
-	// This could be optimised.
-	for y := 0; y < g.gridHeight(); y++ {
-		for x := 0; x < g.gridWidth(); x++ {
-			// For each tile, check all adjacent tiles.
-			if y > 0 && (g.grid[y][x] == g.grid[y-1][x] || g.grid[y-1][x] == 0) {
-				return false
-			}
-			if y < g.gridHeight()-1 && (g.grid[y][x] == g.grid[y+1][x] || g.grid[y+1][x] == 0) {
-				return false
-			}
-			if x > 0 && (g.grid[y][x] == g.grid[y][x-1] || g.grid[y][x-1] == 0) {
-				return false
-			}
-			if x < g.gridWidth()-1 && (g.grid[y][x] == g.grid[y][x+1] || g.grid[y][x+1] == 0) {
-				return false
-			}
-		}
-	}
-	return true
 }
 
 func (g *game) gridHeight() int {
@@ -205,9 +229,23 @@ func (g *game) gridWidth() int {
 	return len(g.grid[0])
 }
 
+func (g *game) newGame() {
+	g.grid = make([][]int, 4)
+	for i := range g.grid {
+		g.grid[i] = make([]int, 4)
+	}
+	// Spawn a tile on a random side.
+	g.move(int(rand.Float64()*4 + 1))
+	g.currentScore = 0
+}
+
 func (g *game) print() {
-	fmt.Printf("\ncli-2048              %07d pts\n", g.currentScore)
-	fmt.Printf("High score:           %07d pts\n\n", g.highScore)
+	// TODO: Save to string then print all in one go, to improve rendering.
+	fmt.Printf("\n            cli-2048          \n  ")
+	for q := 0; q <= 8-len(strconv.Itoa(g.currentScore)); q++ {
+		fmt.Print(" ")
+	}
+	fmt.Printf("🎮 %d || %d 🏆\n\n  ", g.currentScore, g.highScore)
 
 	for _, row := range g.grid {
 		for i := range []int{0, 1, 2} {
@@ -217,69 +255,71 @@ func (g *game) print() {
 					if val == 0 {
 						tilePrinter("   .   ")
 					} else {
-						tilePrinter(" %05d ", val)
+						var tileNumLength = len(strconv.Itoa(val))
+						var rightPadding = (5 - tileNumLength) / 2
+						var isEven = 1 - tileNumLength%2
+						var leftPadding = rightPadding + isEven
+						for q := 0; q <= leftPadding; q++ {
+							tilePrinter(" ")
+						}
+						tilePrinter("%d", val)
+						for q := 0; q <= rightPadding; q++ {
+							tilePrinter(" ")
+						}
 					}
 				} else {
 					tilePrinter("       ")
 				}
 			}
-			fmt.Print("\n")
+			fmt.Print("\n  ")
 		}
 	}
 
-	fmt.Printf("\n     ←,↑,→,↓ or ctrl-q   \n\n")
-
 	if g.isGameOver {
-		fmt.Println("     >>> GAME OVER! <<<    ")
+		fmt.Print("\n  ----------------------------")
+		fmt.Print("\n    >>> 💀 GAME OVER! 💀 <<<  ")
+		fmt.Print("\n  ----------------------------\n")
 	}
+
+	fmt.Printf("\n   ←,↑,→,↓  💾ctrl-c 🔄ctrl-n \n\n")
 }
 
 func getTilePrinter(tile int) func(format string, a ...interface{}) (n int, err error) {
 	c := color.New()
-	if tile == 2 {
+	c.Add(color.FgWhite)
+	switch tile {
+	case 2:
 		c.Add(color.BgRed)
-	}
-	if tile == 4 {
+	case 4:
 		c.Add(color.BgGreen)
-	}
-	if tile == 8 {
+	case 8:
 		c.Add(color.BgYellow)
-	}
-	if tile == 16 {
+	case 16:
 		c.Add(color.BgBlue)
-	}
-	if tile == 32 {
+	case 32:
 		c.Add(color.BgMagenta)
-	}
-	if tile == 64 {
+	case 64:
 		c.Add(color.BgCyan)
-	}
-	if tile == 128 {
+	case 128:
 		c.Add(color.BgWhite)
-	}
-	if tile == 256 {
-		c.Add(color.BgHiBlack)
-	}
-	if tile == 512 {
+		c.Add(color.FgBlack)
+	case 256:
+		c.Add(color.BgBlack)
+	case 512:
 		c.Add(color.BgHiRed)
-	}
-	if tile == 1024 {
+	case 1024:
 		c.Add(color.BgHiGreen)
-	}
-	if tile == 2048 {
+	case 2048:
 		c.Add(color.BgHiYellow)
-	}
-	if tile == 4096 {
+	case 4096:
 		c.Add(color.BgHiBlue)
-	}
-	if tile == 8192 {
+	case 8192:
 		c.Add(color.BgHiMagenta)
-	}
-	if tile == 16384 {
+	case 16384:
 		c.Add(color.BgHiCyan)
-	}
-	if tile == 32768 {
+	case 32768:
 		c.Add(color.BgHiWhite)
+		c.Add(color.FgBlack)
 	}
 	return c.Printf
 }
