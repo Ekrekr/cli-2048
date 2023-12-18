7915b61c0be6f6f3e66efbcdf1655fda505c8b8a
Ekrekr <eliaskassell@gmail.com>
2023-12-18 09:01:54 +0000

    Mysterious diff

--- cli-tetris/cli-tetris.go
+++ cli-tetris/cli-tetris.go
@@ -17,34 +17,41 @@ import (
 
 const (
 	// Types of pieces.
-	PIECE_L         = 0
-	PIECE_L_REVERSE = 1
-	PIECE_COUNT     = 2 // Num of pieces in total.
+	PIECE_L     = 0
+	PIECE_R     = 1
+	PIECE_COUNT = 2 // Num of pieces in total.
 )
 
-const kGridWidth = 16
-const kGridHeight = 48
+const kGridWidth = 10
+const kGridHeight = 19
 
 type Coord struct {
-	y uint32
-	x uint32
+	Y uint32
+	X uint32
 }
 
-// Pieces are stored in a smaller grid representation.
-var pieces = map[uint32][]Coord{
-	PIECE_L:         {{0, 0}, {0, 1}, {0, 2}, {0, 3}, {1, 0}},
-	PIECE_L_REVERSE: {{1, 0}, {1, 1}, {1, 2}, {1, 3}, {0, 0}},
+type Piece struct {
+	ActiveCoords []Coord
+	Color        color.Attribute
+	Width        uint32
+	Height       uint32
+}
+
+// Pieces here are stored with zeroed coords.
+var pieces = map[uint32]Piece{
+	PIECE_L: {ActiveCoords: []Coord{{0, 0}, {0, 1}, {0, 2}, {1, 0}}, Color: color.BgBlue, Width: 3, Height: 2},
+	PIECE_R: {ActiveCoords: []Coord{{1, 0}, {1, 1}, {1, 2}, {0, 0}}, Color: color.BgGreen, Width: 3, Height: 2},
 }
 
 // Variables are exported to make reading from file possible.
 type game struct {
 	CurrentScore      uint32
 	HighScore         uint32
-	Grid              [kGridWidth][kGridHeight]uint32
+	Grid              [kGridHeight][kGridWidth]color.Attribute
 	IsGameOver        bool
 	NeedScreenRefresh bool
 	UpcomingPieces    []uint32
-	ActivePiece       []Coord
+	ActivePiece       Piece
 	DebugMode         bool
 }
 
@@ -52,7 +59,12 @@ func main() {
 	rand.Seed(time.Now().UnixNano())
 
 	var g = game{}
+
+	// TODO: Load save instead.
 	// g.loadSave()
+	g.newGame()
+	g.NeedScreenRefresh = true
+	g.IsGameOver = false
 
 	defer startKeyboard()
 
@@ -66,18 +78,12 @@ func main() {
 			g.NeedScreenRefresh = false
 		}
 		// Error is ignored because combinations like shift + arrow key will throw.
-		_, key, _ := keyboard.GetKey()
+		charRune, key, _ := keyboard.GetKey()
 		switch key {
-		case keyboard.KeyArrowUp:
-			// Drop piece.
-			// Swap to new peice.
-			// Refresh piece store.
-		case keyboard.KeyArrowDown:
-			// Move piece down 1.
 		case keyboard.KeyArrowLeft:
-			// Rotate left.
+			// Move left.
 		case keyboard.KeyArrowRight:
-			// Rotate right.
+			// Move right.
 		case keyboard.KeyCtrlN:
 			g.newGame()
 			g.NeedScreenRefresh = true
@@ -87,28 +93,54 @@ func main() {
 			g.createSave()
 			play = false
 		}
-		g.IsGameOver = g.checkIsGameOver()
+		// TODO: Verify this works.
+		if !g.IsGameOver {
+			switch charRune {
+			case []rune("w")[0]:
+				// Drop piece.
+				// Swap to new peice.
+				// Refresh piece store.
+			case []rune("s")[0]:
+				// Move down.
+				for _, oldCoord := range g.ActivePiece.ActiveCoords {
+					// TODO: Place based on timer instead.
+					if g.Grid[oldCoord.Y+1][oldCoord.X] != color.Reset {
+						g.spawnNewPiece()
+						continue
+					}
+				}
+				for i, oldCoord := range g.ActivePiece.ActiveCoords {
+					newCoord := Coord{Y: oldCoord.Y + 1, X: oldCoord.X}
+					g.Grid[newCoord.Y][oldCoord.X] = g.Grid[oldCoord.Y][oldCoord.X]
+					g.ActivePiece.ActiveCoords[i] = newCoord
+				}
+			case []rune("a")[0]:
+				// // Move left.
+				// for _, oldCoord := range g.ActivePiece.ActiveCoords {
+				// 	newX := oldCoord.X - 1
+				// 	// TODO: Place based on timer instead.
+				// 	if newX < 0 || g.Grid[oldCoord.Y][newX] != color.Reset {
+				// 		return
+				// 	}
+				// }
+				// for _, oldCoord := range g.ActivePiece.ActiveCoords {
+				// 	g.Grid[oldCoord.Y][oldCoord.X-1] = g.Grid[oldCoord.Y][oldCoord.X]
+				// }
+			case []rune("d")[0]:
+				// Move right.
+			case []rune("q")[0]:
+				// Rotate left.
+			case []rune("e")[0]:
+				// Rotate right.
+			}
+		}
 	}
-}
-
-func startKeyboard() func() {
-	if err := keyboard.Open(); err != nil {
-		panic(err)
-	}
-	return func() {
-		_ = keyboard.Close()
-	}
-}
-
-func (g *game) checkIsGameOver() bool {
-	// Check if piece is above top.
-	return false
 }
 
 func (g *game) newGame() {
-	g.Grid = [kGridWidth][kGridHeight]uint32{}
+	g.Grid = [kGridHeight][kGridWidth]color.Attribute{}
 	for i := range [kGridWidth]int{} {
-		g.Grid[i] = [kGridHeight]uint32{}
+		g.Grid[i] = [kGridWidth]color.Attribute{}
 	}
 	for range [5]int{} {
 		g.UpcomingPieces = append(g.UpcomingPieces, rand.Uint32()%PIECE_COUNT)
@@ -120,7 +152,21 @@ func (g *game) newGame() {
 }
 
 func (g *game) spawnNewPiece() {
-	g.ActivePiece = pieces[g.UpcomingPieces[0]]
+	newPiece := pieces[g.UpcomingPieces[0]]
+	newPiece.ActiveCoords = []Coord{}
+	// Place the piece from the localised piece shape grid onto the main game grid.
+	for _, tile := range newPiece.ActiveCoords {
+		gridCoord := Coord{X: (kGridWidth-newPiece.Width)/2 + tile.X, Y: tile.Y}
+		if (g.Grid[gridCoord.X][gridCoord.Y]) != color.Reset {
+			// Reset indicates a populated tile; the new piece cannot fit on
+			// the board so the game is over.
+			g.IsGameOver = true
+		} else {
+			g.Grid[gridCoord.X][gridCoord.Y] = newPiece.Color
+		}
+		newPiece.ActiveCoords = append(newPiece.ActiveCoords, gridCoord)
+	}
+	g.ActivePiece = newPiece
 	g.UpcomingPieces = g.UpcomingPieces[1:]
 	g.UpcomingPieces = append(g.UpcomingPieces, rand.Uint32()%PIECE_COUNT)
 }
@@ -134,12 +180,25 @@ func (g *game) getGameDisplay() string {
 	}
 	output += fmt.Sprintf("🎮 %d || %d 🏆\n\n  ", g.CurrentScore, g.HighScore)
 
-	for _, row := range g.Grid {
-		output += "  " // Left padding.
-		for _, colorCode := range row {
-			printTile(colorCode)
-		}
+	borderTile := printTile(color.BgHiBlue)
+	emptyTile := "  "
+
+	for range [kGridWidth + 2]int{} {
+		output += borderTile
 	}
+	output += "\n"
+	for _, row := range g.Grid {
+		output += emptyTile + borderTile // Left padding and border.
+		for _, colorCode := range row {
+			output += printTile(colorCode)
+		}
+		output += borderTile + "\n"
+	}
+	output += emptyTile
+	for range [kGridWidth + 2]int{} {
+		output += borderTile
+	}
+	output += "\n"
 
 	if g.IsGameOver {
 		output += fmt.Sprintf("\n  ----------------------------")
@@ -150,8 +209,8 @@ func (g *game) getGameDisplay() string {
 	output += fmt.Sprintf("\n   ←,↑,→,↓  💾ctrl-c 🔄ctrl-n \n\n")
 
 	if g.DebugMode {
-		output += fmt.Sprintf("\nActive piece: %v\n", g.ActivePiece)
-		output += fmt.Sprintf("Upcoming pieces: %v\n", g.UpcomingPieces)
+		output += fmt.Sprintf("\nActive piece: %+v\n", g.ActivePiece)
+		output += fmt.Sprintf("Upcoming pieces: %+v\n", g.UpcomingPieces)
 	}
 
 	return output
@@ -204,30 +263,24 @@ func getSavePath() string {
 	return filepath.Join(dir, "Documents", ".2048-save")
 }
 
-func printTile(colorCode uint32) string {
+// Note that tiles are two spaces wide, to make them appear square in a console.
+func printTile(col color.Attribute) string {
 	c := color.New()
-	c.Add([]color.Attribute{
-		color.BgRed,
-		color.BgGreen,
-		color.BgYellow,
-		color.BgBlue,
-		color.BgMagenta,
-		color.BgCyan,
-		color.BgBlack,
-		color.BgHiRed,
-		color.BgHiGreen,
-		color.BgHiYellow,
-		color.BgHiBlue,
-		color.BgHiMagenta,
-		color.BgHiCyan,
-		color.BgHiWhite,
-		color.FgBlack,
-	}[colorCode])
-	return c.Sprintf(" ")
+	c.Add(col)
+	return c.Sprintf("  ")
 }
 
 func clearScreen() {
 	cmd := exec.Command("clear") // Only works for unix based systems.
 	cmd.Stdout = os.Stdout
 	cmd.Run()
+}
+
+func startKeyboard() func() {
+	if err := keyboard.Open(); err != nil {
+		panic(err)
+	}
+	return func() {
+		_ = keyboard.Close()
+	}
 }
---
+++ go.mod
@@ -0,0 +1,11 @@
+module github.com/Ekrekr/cli-2048
+
+go 1.19
+
+require (
+	github.com/eiannone/keyboard v0.0.0-20220611211555-0d226195f203 // indirect
+	github.com/fatih/color v1.13.0 // indirect
+	github.com/mattn/go-colorable v0.1.9 // indirect
+	github.com/mattn/go-isatty v0.0.14 // indirect
+	golang.org/x/sys v0.0.0-20220520151302-bc2c85ada10a // indirect
+)
